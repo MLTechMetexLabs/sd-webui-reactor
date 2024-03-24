fbee1b9b47e073703ed12aadbf5c2d69947d7a0a
Muhammad Rehan Aslam <ranareehanmetex@gmail.com>
2024-03-24 06:50:19 +0500

    origina

--- scripts/reactor_logger.py
+++ scripts/reactor_logger.py
@@ -3,11 +3,14 @@ import copy
 import sys
 
 from modules import shared
+from scripts.reactor_globals import IS_RUN
+from scripts.reactor_helpers import addLoggingLevel
 
 
 class ColoredFormatter(logging.Formatter):
     COLORS = {
         "DEBUG": "\033[0;36m",  # CYAN
+        "STATUS": "\033[38;5;173m",  # Calm ORANGE
         "INFO": "\033[0;32m",  # GREEN
         "WARNING": "\033[0;33m",  # YELLOW
         "ERROR": "\033[0;31m",  # RED
@@ -24,18 +27,29 @@ class ColoredFormatter(logging.Formatter):
 
 
 # Create a new logger
-logger = logging.getLogger("roop")
+logger = logging.getLogger("ReActor")
 logger.propagate = False
+
+# Add Custom Level
+addLoggingLevel("STATUS", logging.INFO + 5)
 
 # Add handler if we don't have one.
 if not logger.handlers:
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(
-        ColoredFormatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ColoredFormatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s","%H:%M:%S")
     )
     logger.addHandler(handler)
 
 # Configure logger
-loglevel_string = getattr(shared.cmd_opts, "controlnet_loglevel", "INFO")
+loglevel_string = getattr(shared.cmd_opts, "reactor_loglevel", "INFO")
 loglevel = getattr(logging, loglevel_string.upper(), "info")
 logger.setLevel(loglevel)
+
+def set_Run(value):
+    global IS_RUN
+    IS_RUN = value
+
+def get_Run():
+    global IS_RUN
+    return IS_RUN
